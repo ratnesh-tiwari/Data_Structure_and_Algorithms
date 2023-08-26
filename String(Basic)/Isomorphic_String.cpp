c1cb992f7f2ab225f1966f42b372a757541653df
Ratnesh Kumar Tiwari <t.ratnesh155@gmail.com>
2023-08-26 19:15:15 +0530

    .

--- String(Basic)/Isomorphic_String.cpp
+++ String(Basic)/Isomorphic_String.cpp
@@ -4,18 +4,18 @@ link- https://leetcode.com/problems/isomorphic-strings/submissions/
 
 */
 
-bool isIsomorphic(string s, string t) {
-        int arr[256] = {0};
-        int arr2[256] = {0};
-        for(int i = 0 ; i < s.size(); i++){
-            if(arr[s[i]] != arr2[t[i]]){
-                return false;
-            }
-            arr[s[i]] = i + 1;
-            arr2[t[i]] = i + 1;
-        }
-        return true;
-    }
+// bool isIsomorphic(string s, string t) {
+//         int arr[256] = {0};
+//         int arr2[256] = {0};
+//         for(int i = 0 ; i < s.size(); i++){
+//             if(arr[s[i]] != arr2[t[i]]){
+//                 return false;
+//             }
+//             arr[s[i]] = i + 1;
+//             arr2[t[i]] = i + 1;
+//         }
+//         return true;
+//     }
 
     ////////////////////////////////////////////
 
